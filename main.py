96a62cbb4d0e63d208dab8f29c0364d5dcddde50
MiPuzmi <cardovamichaela@gmail.com>
2023-11-26 12:10:39 +0100

    k publikování

--- main.py
+++ main.py
@@ -10,62 +10,64 @@ WEST = 14.224437
 EAST = 14.706788
 
 
-# showing the results on the map
-moje_mapa = folium.Map(location=(50.07653, 14.40232), zoom_start=10, tiles="openstreetmap")
+#vytvoření objektu
+interest = PointOfOpportunity(n=NORTH, w=WEST, s=SOUTH, e=EAST, filename='soubory/Cukrarny_all_v3_20231111.csv')
 
-#create object
-interest = PointOfOpportunity(filename='soubory/Cukrarny_all_v3_20231111.csv')
-
+#použití metody mřížky
 point = interest.grid()
 
+#přidání k výsledkům nejbližší zastávku MHD
 point_mhd = interest.distance_mhd(point, 'soubory/stops_MHD_PRAHA.json')
 
+# omezení na oblast Prahy pomocí polygonu
 novy_grid = interest.filter_polygon(point_mhd,"soubory/PRAHA_P.json")
 
+# uložení do souboru
 interest.to_csv(novy_grid, "soubory/novy_grid.csv")
 
-    
-# draw each point of the grid on the map as a rectangle, the colors are graded according to the smallest distance
+# zobrazení výsledků na mapě pomocí knihovny Folium
+moje_mapa = folium.Map(location=(50.07653, 14.40232), zoom_start=10, tiles="openstreetmap")
+
+# zobrazení každého bodu na mřížce jako obdélníku, body jsou barevně odlišeny podle vzdáleností od nejbližší cukrárny
 for index, row in novy_grid.iterrows():
   if row['distance'] < 1:
-    color = "#FFB997"
+    color = "#B5F1CC"
   elif row['distance'] < 3:
-    color = "#F67E7D"
+    color = "#8DDFCB"
   else:
-    color = "#843B62"
+    color = "#5DC7AF"
   # radius = 500
   dx = 0.0025
   folium.Rectangle(
       bounds=[[row['latitude'] + dx, row['longitude'] + dx], [row['latitude'] - dx, row['longitude'] - dx]],
-      # radius=radius,
       color=color,
       weight=1,
       fill_opacity=0.6,
       opacity=1,
       fill_color=color,
-      fill=False,  # gets overridden by fill_color
+      fill=False,  
       popup=row['distance'],
       tooltip="km",
   ).add_to(moje_mapa)
 
-#draw interest as a circle
+#zobrazení cukráren na mapě jako puntík
 for c in interest.dataset:
   c = c.split(",")
   radius = 200
   folium.Circle(
       location=[float(c[4].strip('"')), float(c[5].strip('"'))],
       radius=radius,
-      color="black",
+      color="#EE81CA",
       weight=1,
       fill_opacity=0.8,
       opacity=1,
-      fill_color= '#0B032D',
-      fill=False,  # gets overridden by fill_color
+      fill_color= '#EE81CA',
+      fill=False,  
       popup=c[0].replace('"', ""),
       tooltip="km",
   ).add_to(moje_mapa)
 
-#zadní souřadnic okraje- může být i polygon
+#zadání souřadnic okraje (vykreslí obdélník kolem Prahy)
   trail_coordinates = [
     (NORTH, EAST),
     (NORTH, WEST),
@@ -74,7 +76,7 @@ for c in interest.dataset:
     (NORTH, EAST),
   ]
 
-#tady si vykreslím hranice, dá se využít polygon
+#tady si vykreslím hranice
 folium.PolyLine(trail_coordinates, tooltip="oblast nahody").add_to(moje_mapa)
 
 #uložení mapy jako internetové stránky, můžeme otevřít v prohlížeči
--- opportunity_point.py
+++ opportunity_point.py
@@ -5,12 +5,12 @@ import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
 
-AMOUNT = 10 #number of random points
-STEP = 50 #distance of points in grid 100 ~ 1KM
-KM = 111 # conversion to km
-MULTI = 10000 # conversion coordinates to integer (for loop)
+AMOUNT = 10 #počet náhodných bodů
+STEP = 50 #vzálenost bodů od sebe (při MULTI 10000 je 100 ~ 1km)
+KM = 111 # převod vzdálenosti dvou bodů na km
+MULTI = 10000 # převod souřadnic na celé číslo (pro použití ve for cyklu)
 
-# columns in csv file
+# sloupce v csv souboru
 LATITUDE = 4
 LONGITUDE = 5
 ID = 0
@@ -35,34 +35,30 @@ class PointOfOpportunity:
             self.load_csv(filename)
             # self.coordinates = self.get_coordinates()
 
+    #vytvoření náhodného bodu
     def get_random_point(self):
         x, y = uniform(self.x1, self.x2), uniform(self.y1, self.y2)
         return x, y
     
-    # def get_point_grid(self, step=STEP):
-    #     point_grid = []
-    #     for y in range(int(self.y1*MULTI), int(self.y2*MULTI), step):
-    #         for x in range(int(self.x1*MULTI), int(self.x2*MULTI), step):
-    #             point_grid.append([x/MULTI,y/MULTI])
-    #     return point_grid
-    
+    #otevření souboru typu json
     def load_json(self, file):
         with open(file, encoding='utf8') as f:
             json_dataset = json.load(f)
         return json_dataset
 
-    #read csv file
+    #otevření csv souboru
     def load_csv(self, file):
         with open(file, encoding='utf8') as f:
             self.dataset = f.readlines()[1:]
 
     
     
-    # distance of two points in km
+    # vzdálenost dvou bodů pomocí geopy knihovny (výpočetně náročné)
     # def distance(self,x1,y1,x2,y2):
     #     c = geopy.distance.geodesic([x1, y1], [x2, y2]).km
     #     return c
     
+    #uložení souřadnic cukráren do souboru, nakonec tuto funkci nevyužíváme
     # def get_coordinates(self):
     #     data = self.dataset
     #     list = []
@@ -71,12 +67,13 @@ class PointOfOpportunity:
     #         list.append([x,y])
     #     return list
 
-    #distance of two points
+    #vzdálenost dvou bodů Pythagorova věta(pro krátké vzdálenosti dostačující)
     def distance(self,x1,y1,x2,y2):
         c = sqrt((x1-x2)**2 + (y1-y2)**2)
         return c
     
-    # create random points and calculation of the shortest distance from our object
+    # vytvoření náhodného bodu a zjištění vzdálenosti od nejbližší cukrárny
+    # můžeme si zadáním points určit počet bodů a pomocí top vybrat několik nejvzdálenějších bodů
     def monte_carlo(self, points=AMOUNT, top=1):
         final_list = []
         for i in range(points):
@@ -93,7 +90,8 @@ class PointOfOpportunity:
         final_list.sort(reverse=True)
         return final_list[:top]
     
-    # create points in the grid and calculation of the shortest distance from our object
+    # vytvoření mřížky bodů a zjištění vzdálenosti od nejbližší cukrárny 
+    # můžeme si určit hustotu mřížky pomocí zadání step
     def grid (self, step=STEP):
         final_list = []
         for y in range(int(self.y1*MULTI), int(self.y2*MULTI), step):
@@ -113,10 +111,9 @@ class PointOfOpportunity:
                 final_list.append(result)
         return final_list
     
-    # def write_json(self,result, filename):
-    #     gdf = geopandas.GeoDataFrame(result, crs="EPSG:4326")
-    #     gdf.to_file(filename, driver="GeoJSON")
 
+
+    #přiřazení k bodům příležitosti nejbližsí zastávku a její vzdálenost
     def distance_mhd(self, result, file_mhd):
         mhd_dataset = self.load_json(file_mhd)
         new_result = []
@@ -137,7 +134,7 @@ class PointOfOpportunity:
         return new_result
                 
 
-    #filter point only in polygon(Prague)
+    #omezení výsledků jen na oblast v polygonu
     def filter_polygon(self, result, polygon_file):
         grid = gpd.GeoDataFrame(result, crs="EPSG:4326")
         polys = gpd.read_file(polygon_file)
@@ -149,7 +146,7 @@ class PointOfOpportunity:
         novy_grid.drop(columns=['Praha'], inplace=True)
         return novy_grid
 
-    #save to csv
+    #uložení do csv souboru
     def to_csv(self, gdf, new_file):
         df = pd.DataFrame(gdf)
         df.to_csv(new_file, index=False)
